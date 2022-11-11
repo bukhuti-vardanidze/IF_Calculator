ae156ad008909eb204d564cc8cdbe138109c7c3e
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2022-11-11 23:10:49 +0400

    Calculation using IF

---
+++ IF_Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter First Number : ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Second Number : ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Choose One Operation : [+] [-] [*] [/] [%]");
+string operations = Console.ReadLine();
+
+if (operations == "+")
+{
+    Console.WriteLine($"{number1}+{number2}= {number1+number2}");
+}
+else if(operations == "-")
+{
+    Console.WriteLine($"{number1}-{number2}= {number1-number2}");
+}
+else if(operations == "*") 
+{
+    Console.WriteLine($"{number1}*{number2}= {number1*number2}");
+}
+else if (operations == "/")
+{
+    Console.WriteLine($"{number1}/{number2}= {number1/number2}");
+}
+else if (operations == "%")
+{
+    Console.WriteLine($"{number1}%{number2}=  {number1%number2}");
+}
+else{
+Console.WriteLine("Choose Correct Option!");
+}
